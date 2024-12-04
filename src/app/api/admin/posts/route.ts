de46aa07fcf87448be498b62a01af0e84ca638f1
小野瑠里香 <rurika@onokounoMacBook-Air.local>
2024-12-05 06:30:26 +0900

    管理者_記事新規作成APIの実装

--- src/app/api/admin/posts/route.ts
+++ src/app/api/admin/posts/route.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client/extension";
 import { NextRequest, NextResponse } from "next/server";
 
-
 const prisma = new PrismaClient()
 
 export const GET = async (request: NextRequest) => {
@@ -28,5 +27,55 @@ export const GET = async (request: NextRequest) => {
   } catch (error) {
     if (error instanceof Error)
       return NextResponse.json({ status:error.message }, { status: 400 })
+  }
+}
+
+// 記事作成のリクエストボディの型
+interface CreatePostRequestBody {
+  title: string
+  content: string
+  categories: { id: number }[]
+  thumbnailUrl: string
+}
+
+// POSTという命名にすることで、POSTリクエストの時にこの関数が呼ばれる
+export const POST = async (request: Request, context: any) => {
+  try {
+    // リクエストのbodyを取得
+    const body = await request.json()
+
+    // bodyの中からtitle, contant, categories, thumbnailUrlを取り出す
+    const { title, content, categories, thumbnailUrl }: CreatePostRequestBody = body
+
+    // 投稿をDBに生成
+    const data = await prisma.post.create({
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+      },
+    })
+
+    // 記事とカテゴリーの中間テーブルのレコードをDBに生成
+    // 本来複数同時生成には、createManyというメソッドがあるが、sqliteではcreateManyが使えないためfor文1つずつ実施する
+    for (const catefory of categories) {
+      await prisma.postCaregory.create({
+        data: {
+          categoryId: catefory.id,
+          postId: data.id,
+        },
+      })
+    }
+
+    // レスポンスを返す
+    return NextResponse.json({
+      status: 'OK',
+      message: '作成しました',
+      id: data.id,
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 })
+    }
   }
 }
