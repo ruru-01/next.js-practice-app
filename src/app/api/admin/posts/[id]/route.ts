0be53cdaaf2b60c3f211f2c139f10490844eb9bc
小野瑠里香 <rurika@onokounoMacBook-Air.local>
2024-12-05 22:43:05 +0900

    管理者_記事更新APIの実装

--- src/app/api/admin/posts/[id]/route.ts
+++ src/app/api/admin/posts/[id]/route.ts
@@ -34,3 +34,61 @@ export const GET = async (
       return NextResponse.json({ status: error.message }, { status: 400 })
   }
 }
+
+// 記事の更新時に送られてくるリクエストのbodyの型
+interface UpdatePostRequestBody {
+  title: string
+  content: string
+  categories: { id: number }[]
+  thumbnailUrl: string
+}
+
+// PUTという命名にすることで、PUTリクエストの時にこの関数が呼ばれる
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }, // ここでリクエストパラメータを受け取る
+) => {
+  // paramsの中にidが入っているため、それを取り出す
+  const { id } = params
+
+  // リクエストのbodyを取得
+  const { title, content, categories, thumbnailUrl }: UpdatePostRequestBody = await request.json()
+
+  try {
+    // idを指定して、Postを更新
+    const post = await prisma.post.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+      },
+    })
+
+    // 一旦記事とカテゴリーの中間テーブルのレコードを全て削除
+    await prisma.postCaregory.deleteMany({
+      where: {
+        postId: parseInt(id),
+      },
+    })
+
+    // 記事とカテゴリーの中間テーブルのレコードをDBに生成
+    // 本来複数同時生成には、createManyというメソッドがあるが、sqliteではcreateManyが使えないためfor文1つずつ実施
+    for (const category of categories) {
+      await prisma.postCaregory.create({
+        data: {
+          postId: post.id,
+          categoryId: category.id,
+        },
+      })
+    }
+
+    // レスポンスを返す
+    return NextResponse.json({ status: 'OK', post: post }, { status: 200 })
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status:400 })
+  }
+}
