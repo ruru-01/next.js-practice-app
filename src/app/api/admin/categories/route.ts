a30329c0d3662fcb301425b0fbc76395321a9834
ruru-01 <harapeco.tv@gmail.com>
2024-12-10 06:28:15 +0900

    管理者_カテゴリー新規作成APIの実装

--- src/app/api/admin/categories/route.ts
+++ src/app/api/admin/categories/route.ts
@@ -1,4 +1,6 @@
+import { createChainedFunction } from "@mui/material";
 import { PrismaClient } from "@prisma/client/extension";
+import { RequestAsyncStorage } from "next/dist/client/components/request-async-storage.external";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient()
@@ -17,5 +19,40 @@ export const GET = async (request: NextRequest) => {
   } catch (error) {
     if (error instanceof Error)
       return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
+
+//カテゴリーの作成時に送られてくるリクエストのbodyの型
+interface CreateCategoryRequestBody {
+  name: string
+}
+
+// POSTという命名にすることで、POSTリクエストの時にこの関数が呼ばれる
+export const POST = async (request: Request, context: any) => {
+  try {
+    // リクエストのbodyを取得
+    const body = await request.json()
+
+    // bodyの中からnameを取り出す
+    const { name }: CreateCategoryRequestBody = body
+
+    // カテゴリーをDBに生成　
+    const data = await prisma.category.create({
+      data: {
+        name,
+      },
+    })
+
+    // 中間レコードはなし
+    // レスポンスを返す
+    return NextResponse.json({
+      status: 'OK',
+      message: '作成しました',
+      id: data.id,
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 })
+    }
   }
 }
